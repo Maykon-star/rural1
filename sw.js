6d179814799b8a4dc2e69ec024415454c831055b
Maykon-star <214259625+Maykon-star@users.noreply.github.com>
2026-08-31 13:00:05 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
 const CACHE_NAME = 'eito-degital-v1';
-const ASSENT_TO_CACHE = [
+const ASSENTS_TO_CACHE = [
     './',
     './index.html',
-    './style.css',
+    './rural.css',
     './manifest.json',
     './icone-192.png',
     './icone.512.png',
     './calculos.js',
-    './script.js'
+    './rural.js'
 ];
 self.addEventListener('install', (event) => {
     event.waitUntil (
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSENT_TO_CACHE);
+            return cache.addAll(ASSENTS_TO_CACHE);
+        })
+    );
+});
+self.addEvenetlistener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || FileSystemDirectoryHandle(event.requewst);
         })
     );
 });
